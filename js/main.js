5340789da57b046230a72713e4fd779bcca3e8f7
infinicky <infinicky-codes@gmail.com>
2025-08-06 11:16:17 +0200

    Chore: switch to local font for GDPR compliancy

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function toggleNav() {
     var navbar = document.getElementById("navbar-links");
     navbar.classList.toggle("show");
-}
+};
 
 // Close menu when navigation link is clicked
 document.addEventListener("DOMContentLoaded", function() {
@@ -9,23 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
     var links = document.querySelectorAll("#navbar-links a");
     links.forEach(function(link) {
         link.addEventListener("click", function() {
-            if (window.innerWidth <= 620) {
-                navbar.classList.remove("show");
-            }
+            navbar.classList.remove("show");
         });
     });
 });
 
 // Close menu when clicked outside
 document.addEventListener("click", function(event) {
-    var navbar = document.getElementById("navbar-links");
+    var nav = document.getElementById("navbar-links");
     var hamburger = document.querySelector(".header-main-hamburger");
-    if (
-        window.innerWidth <= 620 &&
-        navbar.classList.contains("show") &&
-        !navbar.contains(event.target) &&
-        !hamburger.contains(event.target)
-    ) {
-        navbar.classList.remove("show");
+    if (nav.classList.contains("show") && !hamburger.contains(event.target)) {
+        nav.classList.remove("show");
     }
 });
